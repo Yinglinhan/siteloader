1d59187a146c16e3c7657ad747564f1c0df2a8bb
MUMUMU <yinglinhan1987@gmail.com>
2023-05-25 15:04:57 +0800

    修改

--- main.js
+++ main.js
@@ -2,8 +2,6 @@
 // import SiteLoader from "https://www.unpkg.com/siteloader/dist/sl.js";
 // import SiteLoader from 'siteloader'
 
-
-console.log(1, SiteLoader)
 const sl = new SiteLoader(
   [
     {
@@ -51,13 +49,7 @@ sl.addEventListener('progress', (e) => {
   console.log(loadingBar.style.transform = `translateX(-${100 - e.progress}%)`)
 
 })
-// sl.addEventListener('stage1', () => {
-//   console.log('stage1结束了111111111111111')
-// })
 
-// sl.addEventListener('stage2', () => {
-//   console.log('stage1结束了2222222222')
-// })
 
 sl.addEventListener('countComplete', () => {
   console.log('8888888888888888888888')
@@ -72,12 +64,3 @@ sl.setTargetTextDom('.loading-num')
 sl.startLoad()
 
 
-// const video = document.querySelectorAll('#video')
-// console.log(video)
-// setTimeout(() => {
-//   video[0].addEventListener("canplay", (event) => {
-//     console.log("Video can start, but not sure it will play through.");
-//   })
-//   video[0].src = video[0].src
-
-// }, 5000)
