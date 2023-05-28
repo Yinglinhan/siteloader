24294af0bcecfcfac44d7afb0de5a447877a0d78
MUMUMU <yinglinhan1987@gmail.com>
2023-05-28 20:42:14 +0800

    添加自定义加载功能

--- dist/sl.js
+++ dist/sl.js
@@ -8,10 +8,10 @@ class l {
    * @param {StageObj[]} stageArr - 特别的 {@link StageObj}
    */
   constructor(e) {
-    this._allResorceData = e, this._allRourceCount = 0, this._allDoms = {
+    this._allResorceData = e, this._allResourceCount = 0, this._allDoms = {
       imgsDoms: [],
       mediaDoms: []
-    }, this._allResourcesTag = {}, this._isStageLoad = e.length > 1, this._stageDoms = [], this._stageResourceCount = {}, this._stageResourceLoadedCount = {}, this._targetTextDom = null, this._targetProgress = 20, this._queneCompareNum = 0, this._loadedCount = 0, this._progress = 0, this._progressChangeTimer = null, this._events = {
+    }, this._allResourcesTag = {}, this._isStageLoad = e && e.length > 1, this._stageDoms = [], this._stageResourceCount = {}, this._stageResourceLoadedCount = {}, this._addonLoadedRatio = 0, this._addonLoadFunc = null, this._targetTextDom = null, this._targetProgress = 20, this._loadedCount = 0, this._progress = 0, this._progressChangeTimer = null, this._events = {
       beforeStart: null,
       countComplete: null,
       trueLoadFinish: null,
@@ -22,9 +22,9 @@ class l {
     }), this._errorCheck();
   }
   _errorCheck() {
-    if (this._allResorceData.length === 0)
-      throw new Error("no resource data | 没有资源数据");
-    if (!(this._allResorceData instanceof Array))
+    if (this._allResorceData && this._allResorceData.length === 0)
+      throw new Error("no resource data | 不能传入空数组，没有资源数据");
+    if (this._allResorceData && !(this._allResorceData instanceof Array))
       throw new Error("传入的必须是一个数组 | resource data must be array");
     if (this._isStageLoad) {
       this._allResorceData.forEach((s) => {
@@ -44,11 +44,11 @@ class l {
       if (new Set(e).size !== this._allResorceData.length)
         throw new Error("stageName 重复 | stageName is repeat");
     } else {
-      if (!this._allResorceData[0].sources)
+      if (this._allResorceData && !this._allResorceData[0].sources)
         throw new Error("没有设置资源数据 sources data | no sources");
-      if (!(this._allResorceData[0].sources instanceof Array))
+      if (this._allResorceData && !(this._allResorceData[0].sources instanceof Array))
         throw new Error("资源数据sources必须是数组 | sources must be array");
-      this._allResorceData[0].sources.forEach((e) => {
+      this._allResorceData && this._allResorceData[0].sources.forEach((e) => {
         if (!["image", "font", "media"].includes(e.sourceType))
           throw new Error("sourceType is not correct | sourceType 名称不对，只能是image font或者media（视频或者音频）");
       });
@@ -59,31 +59,36 @@ class l {
     this._targetTextDom = t;
   }
   startLoad() {
-    this._events.beforeStart && this._events.beforeStart(), this._loaderInit(), this._progressDetect();
+    this._events.beforeStart && this._events.beforeStart(), this._loaderInit(), this._progressDetect(), this._addonLoadFunc && this._addonLoadFunc();
   }
   _errorLoad(e, t) {
     this._loadedCount++, this._trueLoadControl(), console.warn(`加载${e}资源${t}失败`);
   }
   _loaderInit() {
-    this._allResorceData.forEach((e) => {
+    this._allResorceData ? this._allResorceData.forEach((e) => {
       this._isStageLoad ? this._stageResourceInit(e) : this._allResourceInit(e);
-    }), this._allResorceData.length > 1 ? this._mutileStageStartLoad(this._allResorceData[0].stageName) : this._allLoad();
+    }) : this._emptyParamInit(), this._allResorceData && this._allResorceData.length > 1 ? this._mutileStageStartLoad(this._allResorceData[0].stageName) : this._allLoad();
   }
-  // 分阶段加载的资源初始化
-  _stageResourceInit(e) {
-    this._stageResourceCount[e.stageName] = 0, this._stageResourceLoadedCount[e.stageName] = 0, e.sources.forEach((t) => {
-      this._stageDoms[e.stageName] = this._stageDoms[e.stageName] || {}, this._stageDoms[e.stageName][t.sourceType] = this._stageDoms[e.stageName][t.sourceType] || [], t.sourceType === "image" ? t.selectors.forEach((s) => {
-        const r = document.querySelectorAll(s);
-        r.forEach((o) => {
-          this._allResourcesTag[o.currentSrc || o.src] = !1;
-        }), this._stageDoms[e.stageName][t.sourceType] = [...this._stageDoms[e.stageName][t.sourceType], ...r], this._allRourceCount += r.length, this._stageResourceCount[e.stageName] += r.length;
-      }) : t.sourceType === "media" && t.selectors.forEach((s) => {
-        const r = document.querySelectorAll(s);
-        r.forEach((o) => {
-          this._allResourcesTag[o.src] = !1;
-        }), this._stageDoms[e.stageName][t.sourceType] = [...this._stageDoms[e.stageName][t.sourceType], ...r], this._allRourceCount += r.length, this._stageResourceCount[e.stageName] += r.length;
-      });
-    });
+  // _checkRepeatSource() {
+  //   // 检查this._allDom.imgsDoms中的资源的src属性是否有重复的
+  //   const allImgsSrc = this._allDom.imgsDoms.map((img) => {
+  //       return img.currentSrc || img.src
+  //   })
+  //   const allImgsSrcSet = new Set(allImgsSrc)
+  //   if (allImgsSrcSet.size !== allImgsSrc.length) {
+  //       // 
+  //   }
+  //   // 检查this._allDom.mediaDoms中的资源的src属性是否有重复的
+  //   const allMediaSrc = this._allDom.mediaDoms.map((media) => {
+  //       return media.currentSrc || media.src
+  //   })
+  //   const allMediaSrcSet = new Set(allMediaSrc)
+  //   if (allMediaSrcSet.size !== allMediaSrc.length) {
+  //   }
+  // }
+  _emptyParamInit() {
+    const e = document.querySelectorAll("img"), t = document.querySelectorAll("video"), s = document.querySelectorAll("audio");
+    console.log(e, t, s), this._allDoms.imgsDoms = Array.from(e), this._allDoms.mediaDoms = Array.from(t).concat(Array.from(s)), this._allResourceCount = this._allDoms.imgsDoms.length + this._allDoms.mediaDoms.length;
   }
   // 整体加载的资源初始化
   _allResourceInit(e) {
@@ -92,12 +97,28 @@ class l {
         const r = document.querySelectorAll(s);
         r.forEach((o) => {
           this._allResourcesTag[o.currentSrc || o.src] = !1;
-        }), this._allDoms.imgsDoms = [...this._allDoms.imgsDoms, ...r], this._allRourceCount += r.length;
+        }), this._allDoms.imgsDoms = [...this._allDoms.imgsDoms, ...r], this._allResourceCount += r.length;
       }) : t.sourceType === "media" && t.selectors.forEach((s) => {
         const r = document.querySelectorAll(s);
         r.forEach((o) => {
           this._allResourcesTag[o.src] = !1;
-        }), this._allDoms.mediaDoms = [...this._allDoms.mediaDoms, ...r], this._allRourceCount += r.length;
+        }), this._allDoms.mediaDoms = [...this._allDoms.mediaDoms, ...r], this._allResourceCount += r.length;
+      });
+    });
+  }
+  // 分阶段加载的资源初始化
+  _stageResourceInit(e) {
+    this._stageResourceCount[e.stageName] = 0, this._stageResourceLoadedCount[e.stageName] = 0, e.sources.forEach((t) => {
+      this._stageDoms[e.stageName] = this._stageDoms[e.stageName] || {}, this._stageDoms[e.stageName][t.sourceType] = this._stageDoms[e.stageName][t.sourceType] || [], t.sourceType === "image" ? t.selectors.forEach((s) => {
+        const r = document.querySelectorAll(s);
+        r.forEach((o) => {
+          this._allResourcesTag[o.currentSrc || o.src] = !1;
+        }), this._stageDoms[e.stageName][t.sourceType] = [...this._stageDoms[e.stageName][t.sourceType], ...r], this._allResourceCount += r.length, this._stageResourceCount[e.stageName] += r.length;
+      }) : t.sourceType === "media" && t.selectors.forEach((s) => {
+        const r = document.querySelectorAll(s);
+        r.forEach((o) => {
+          this._allResourcesTag[o.src] = !1;
+        }), this._stageDoms[e.stageName][t.sourceType] = [...this._stageDoms[e.stageName][t.sourceType], ...r], this._allResourceCount += r.length, this._stageResourceCount[e.stageName] += r.length;
       });
     });
   }
@@ -172,8 +193,14 @@ class l {
   }
   // 用来控制加载进度目标的方法 
   _trueLoadControl() {
-    const e = this._loadedCount / this._allRourceCount;
-    (e * 100 - this._queneCompareNum > 25 || e === 1) && (this._queneCompareNum = e * 100, this._targetProgress = Math.ceil(e * 60) + 20, e === 1 && (this._targetProgress = 100, this._events.trueLoadFinish && this._events.trueLoadFinish()));
+    const e = this._loadedCount / this._allResourceCount;
+    this._addonLoadFunc ? (this._targetProgress = Math.ceil(e * 40) + Math.ceil(this._addonLoadedRatio * 30) + 20, e === 1 && this._addonLoadedRatio === 1 && (this._targetProgress = 100, this._events.trueLoadFinish && this._events.trueLoadFinish())) : (this._targetProgress = Math.ceil(e * 60) + 20, e === 1 && (this._targetProgress = 100, this._events.trueLoadFinish && this._events.trueLoadFinish()));
+  }
+  setAddonLoadFunc(e) {
+    this._addonLoadFunc = e;
+  }
+  setAddonLoadData(e) {
+    this._addonLoadedRatio = e, this._trueLoadControl();
   }
   // 整个加载进度包括计数到100之后调用的方法
   _loadFinish() {
--- main.js
+++ main.js
@@ -3,23 +3,31 @@
 // import SiteLoader from 'siteloader'
 import SiteLoader from "./lib/siteLoader"
 
+import * as THREE from 'three'
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+
+var loadingManager = new THREE.LoadingManager();
+console.log(loadingManager)
+
+
+
 const sl = new SiteLoader(
-  [
-    {
-      sources: [
-        {
-          sourceType: 'image',
-          selectors: ['.selected', '.ppp']
-        },
-        {
-          sourceType: 'media',
-          selectors: ['#video']
-        }
-      ]
+  // [
+  //   {
+  //     sources: [
+  //       {
+  //         sourceType: 'image',
+  //         selectors: ['.selected', '.ppp']
+  //       },
+  //       {
+  //         sourceType: 'media',
+  //         selectors: ['#video']
+  //       }
+  //     ]
 
-    },
+  //   },
 
-  ]
+  // ]
 )
 
 const loadingBar = document.querySelector('.loading-color')
@@ -31,18 +39,52 @@ sl.addEventListener('progress', (e) => {
 })
 
 sl.addEventListener('countComplete', () => {
+  console.log(999999999999)
   setTimeout(() => {
     document.body.style.overflow = 'auto'
-  document.querySelector('.loading-container').style.display = 'none'
-  },200)
+    document.querySelector('.loading-container').style.display = 'none'
+  }, 200)
+})
+
+sl.addEventListener('trueLoadFinish', () => {
+  console.log(999999)
 })
 
 
 sl.setTargetTextDom('.loading-num')
 
-sl.progressSpeed = 50
+sl.progressSpeed = 80
 
 sl.needSpeedUp = true
+
+
+sl.setAddonLoadFunc(
+
+  () => {
+    console.log(99)
+    // 监听加载完成事件
+    loadingManager.onLoad = function () {
+      console.log("加载完成！");
+    };
+
+    // 监听加载进度事件
+    loadingManager.onProgress = function (item, loaded, total) {
+      console.log("正在加载 " + item + " (" + loaded + "/" + total + ")");
+      sl.setAddonLoadData(loaded / total)
+
+    };
+
+    // 加载模型
+    var loader = new OBJLoader(loadingManager);
+    loader.load('/resources/monkey.obj', function (object) {
+      console.log(11)
+    });
+  }
+
+)
+
 sl.startLoad()
+
+
 
 
