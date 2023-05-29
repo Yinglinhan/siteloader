83d210a329b1ff079eb4e88efedf412da89eb330
MUMUMU <yinglinhan1987@gmail.com>
2023-05-29 21:48:06 +0800

    修改

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,6 +1,38 @@
 declare module 'siteloader' {
   export default class SiteLoader {
-    constructor(data?: any[] | undefined);
+    constructor(data?: stageResourceData[] | resourceData[]);
+    
+    
+    progressSpeed:number;
 
+    needSpeedUp:boolean;
+
+    setTargetTextDom(selector: string): void;
+
+    addEventListener(type: EventType, listener: (event?: ProgressEvent) => void): void;
+    
+    setAddonLoadData(ratio: number): void;
+
+    @MyDescription('设置附加加载函数')
+    setAddonLoadFunc(func: () => void): void;
+      
+
+    @MyDescription('开始进行资源加载进度统计')
+    startLoad(): void;
   }
+
+  type EventType = 'beforeStart' | 'countComplete' | 'trueLoadFinish' | 'progress' | string;
+
+  interface stageResourceData {
+    stageName:string;
+    sources: resourceData[];
+  }
+
+  interface resourceData {
+    sourceType:  'media' | 'image';
+    selectors: string[];
+  }
+
+
+
 }
